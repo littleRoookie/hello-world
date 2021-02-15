21d48a2b1fcfd5a025d89507b8f1eedbb7664749
gzx_glb <gzx_glb@uestc.com>
2021-02-15 23:33:06 +0800

    尝试111

---
+++ test1/src/cn/uestc/Test2.java
@@ -0,0 +1,12 @@
+package cn.uestc;
+
+/**
+ * @author Zhengxing Guan
+ * @time 2021-02-15 23:32
+ * @description
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("这是新的Test");
+    }
+}
