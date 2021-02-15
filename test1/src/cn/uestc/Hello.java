ada9a1d5b859abe8606d1160a443f4d6c7d46970
gzx_glb <gzx_glb@uestc.com>
2021-02-15 22:30:23 +0800

    尝试

---
+++ test1/src/cn/uestc/Hello.java
@@ -0,0 +1,12 @@
+package cn.uestc;
+
+/**
+ * @author Zhengxing Guan
+ * @time 2021-02-15 22:25
+ * @description
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
